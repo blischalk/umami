6b0587498ce8bae632be60cb90483ec0ab6021df
Brett Lischalk <blischalk@opinionlab.com>
2017-01-23 10:19:06 -0600

    Read bytes from curl into memory

--- get.c
+++ get.c
@@ -1,10 +1,11 @@
 /**
  * https://dev.twitter.com/rest/reference/get/statuses/user_timeline
  * http://pixelrobotics.com/2013/01/consuming-the-twitter-public-stream-with-libcurl/
+ * https://curl.haxx.se/libcurl/c/getinmemory.html
  *
  * Compile with:
  *
- * gcc -o get -loauth -lcurl get.c
+ * gcc -o get get.c -loauth -lcurl
  **/
 
 #include <stdlib.h>
@@ -12,6 +13,31 @@
 #include <unistd.h>
 #include <oauth.h>
 #include <curl/curl.h>
+
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if(mem->memory == NULL) {
+    /* out of memory! */
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+
+  return realsize;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -35,8 +61,15 @@ int main(int argc, const char *argv[])
     const char *atok = getenv("ATOK");
     const char *atoksecret = getenv("ATOKSECRET");
 
+
+    struct MemoryStruct chunk;
+
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
+    chunk.size = 0;    /* no data at this point */
+
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl = curl_easy_init();
+
 
     const char *url = "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=blischalk&count=10";
 
@@ -52,16 +85,33 @@ int main(int argc, const char *argv[])
     // libcurl will now fail on an HTTP error (>=400)
     curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
 
-    // In this case, we're not specifying a callback function for
-    // handling received data, so libcURL will use the default, which
-    // is to write to the file specified in WRITEDATA
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)out);
+    /* send all data to this function  */
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
-    // Execute the request!
-    int curlstatus = curl_easy_perform(curl);
-    printf("curl_easy_perform terminated with status code %d\n", curlstatus);
+    /* we pass our 'chunk' struct to the callback function */
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+    /* get it! */
+    int res = curl_easy_perform(curl);
+
+    /* check for errors */
+    if(res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+    }
+    else {
+      /*
+       * Now, our chunk.memory points to a memory block that is chunk.size
+       * bytes big and contains the remote file.
+       *
+       * Do something nice with it!
+       */
+
+      printf("%lu bytes retrieved\n", (long)chunk.size);
+    }
 
     curl_easy_cleanup(curl);
+    free(chunk.memory);
     curl_global_cleanup();
     fclose(out);
 
